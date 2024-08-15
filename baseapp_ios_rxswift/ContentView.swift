845a7b84ba1cb736ec77f2a3ef2d8a87ded9e435
vumanhtu <tu.vumanh@apuscorp.com>
2024-08-15 11:51:54 +0700

    Initial Commit

---
+++ baseapp_ios_rxswift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  baseapp_ios_rxswift
+//
+//  Created by MacBook Pro Của A Tú on 15/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ baseapp_ios_rxswift/baseapp_ios_rxswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  baseapp_ios_rxswiftApp.swift
+//  baseapp_ios_rxswift
+//
+//  Created by MacBook Pro Của A Tú on 15/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct baseapp_ios_rxswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
